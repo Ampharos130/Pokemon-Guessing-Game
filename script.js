e9dfa77458dfa18dea406e50f89e48f757a89cd2
Timmy Do <timmy.do130@gmail.com>
2021-12-12 19:21:18 -0700

    Add title and input and button

---
+++ script.js
@@ -0,0 +1,28 @@
+// POKEMON GUESSING GAME
+
+//create a variable 
+//request api from pokemon site
+// make a submit and input
+// randomly generate one of the pokemon ID's
+// type the correct pokemon's name to win
+
+
+//variables
+
+
+
+//element references
+
+
+
+// event listeners
+
+
+
+
+//functions
+
+
+
+
+
