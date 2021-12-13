d2a8e2b88d0a8bf45b66ac4aefd3e7bd0f966350
Timmy Do <timmy.do130@gmail.com>
2021-12-13 14:03:52 -0700

    put pokemon list in an 2d array

--- script.js
+++ script.js
@@ -8,30 +8,54 @@
 
 
 //variables
-const $URL =`https://pokeapi.co/api/v2/pokemon/`
+const url =`https://pokeapi.co/api/v2/pokemon?limit=151`;
+let totalPokemon = [];
 
 
 //element references
-const $input = $(`form`);
-const $form = $(`input[type="text"]`);
+const $button = $(`button`);
+const $input = $(`input[type="text"]`);
 
 
 // event listeners
-$form.on(`submit`, handleGetData);
-
+$button.on(`click`, handleGetData);
+startup();
 
 
 //functions
+// load the data
+
+function startup(){
+    $.ajax(url).then(
+        function (data){
+         //    render(data);
+         data.results.forEach(element =>{
+             let temp = [];
+             
+             temp.push(element.name);
+             temp.push(element.url);
+             totalPokemon.push(temp)
+         })
+         console.log(totalPokemon)
+        },
+        function (error) {
+            console.log(`something went wrong`);
+            console.log(error);
+        }
+    );
+}
 
 function handleGetData(evt) {
+    evt.preventDefault()
    const userInput = $input.val();
-   if (!userInput) return;
+//    if (!userInput) return;
 
    $input.val(``);
 
-   $.ajax(URL + userInput).then(
+   $.ajax(url + userInput).then(
        function (data){
-           render(data);
+        //    render(data);
+        console.log(data)
        },
        function (error) {
            console.log(`something went wrong`);
@@ -40,8 +64,6 @@ function handleGetData(evt) {
    );
 }
 
-function render(pokemonName){
-
-}
 
 
+// choose a random pokemon
