e3cc6f52adc6c14f0526c8070b996e783a4ad16d
Timmy Do <95107042+Ampharos130@users.noreply.github.com>
2021-12-16 09:52:40 -0800

    Add files via upload
    Sudo code some code to explain code I've written

--- script.js
+++ script.js
@@ -24,18 +24,18 @@ then I want computer to tell me I got name correct and its equal to name I type
 let mainLink = `https://pokeapi.co/api/v2/pokemon?limit=151`
 let totalPk
 let detailData  
+
 //element references
-let $button = $(`button`);
+let $button = $(`button`); //selecting DOM elements to work with in js
 let $input = $(`input`);
 let $img = $(`.guessImg`);
 
-// initial statement
+// initial statement, runs function straight away so it can have a list loaded in for other functions to work
 loadFirst();
-
-
 
 //functions
 
+// Loads the first pokemon api's list of names and url
 function loadFirst(){
 $.ajax(mainLink).then(function (data){
     
@@ -43,14 +43,13 @@ $.ajax(mainLink).then(function (data){
      choosePokemon();
 });
 }
-
-
-
+// generates a random number for the index
 function generateIndex(){
  
     return Math.floor(Math.random() * ( totalPk.results.length - 0 + 1)) + 0;
 }
-
+// takes the generated number and uses it as a index to pull information from array
+// ajax into another url for more information on the pokemon and pulls even more data
  function choosePokemon(){
     // select the index to get access for the pokemon
     $.ajax(totalPk.results[generateIndex()]).then(function (detailedData){
@@ -59,12 +58,14 @@ function generateIndex(){
     });
 
  }
- 
+ // renders api's official artwork img onto webpage
  function render (pokeData){
     
     $(`div`).html(`<img src = "${detailData.sprites.other["official-artwork"].front_default}">`)
  }
 
+ // event listener
+ // truthy or falsey, checks to see if user input is equal to api's 
  $button.on(`click`, function(evt){
      evt.preventDefault();
      
@@ -80,10 +81,9 @@ function generateIndex(){
      }
  })
 
+ // reveal hidden pokemon behind img
  $img.on(`click`, function(){
      render()
  })
  //when correct the page reloads
 
-//  official-artwork
-//  name
